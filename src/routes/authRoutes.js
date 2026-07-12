16fc80ae00ef18aa3c26286dd4cd21f03c2dbfb7
Habishek <habishekiosdevloper@gmail.com>
2026-07-12 23:24:35 +0530

    Updated few errors

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
+<<<<<<< HEAD
 const { requireAuth } = require('../middleware/auth');
+=======
+>>>>>>> bde03ee (Updated few errors)
 const { register, login, me, changePassword } = require('../controllers/authController');
-router.post('/change-password', requireAuth, changePassword);
+const { requireAuth } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', requireAuth, me);
+router.post('/change-password', requireAuth, changePassword);
 
 module.exports = router;
